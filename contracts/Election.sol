de0bdf1ec7a8027f0e89fa562151d3158f772aba
Ythalo Rossy Saldanha Lira <ythalorossy@gmail.com>
2018-09-06 22:50:37 -0400

    3. Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,8 @@ contract Election {
         uint voteCount;
     }
 
+    // Store account that have voted
+    mapping(address => bool) public voters;
     // Store Candidates
     // Fetch Candidates
     mapping(uint => Candidate) public candidates;
@@ -22,5 +24,19 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a vlid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // Update candidate vote Count
+        candidates[_candidateId].voteCount++;
     }
 }
